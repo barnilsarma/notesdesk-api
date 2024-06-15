e38ec9d8b0f7b64dda048925409274318b3fab07
Barnil Sarma <barnilsarma@gmail.com>
2024-06-16 02:49:34 +0530

    few changes

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 import express from "express";
 import * as controllers from "./src/controllers/index.js";
-import * as middlewares from "./src/middlewares/index.js";                                     
+import * as middlewares from "./src/middlewares/index.js";
 import dotenv from 'dotenv';
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 //
 dotenv.config();
+app.use(cors({ origin: `${process.env.FRONTEND_URL}` }));
 
 app.post('/library/add', middlewares.validateLibrary, controllers.library.create);
 app.get('/library/read', controllers.library.read);
 app.put('/library/update/:id', controllers.library.update);
 app.put('/library/delete/:id', controllers.library.deleteLib);
 //
-app.post('/subject/add',middlewares.validateSubject,  controllers.subject.create);
+app.post('/subject/add', middlewares.validateSubject, controllers.subject.create);
 app.get('/subject/read/:libraryId', controllers.subject.read);
 app.put('/subject/update/:id', controllers.subject.update);
 app.put('/subject/delete/:id', controllers.subject.deleteSub);
 //
-app.post('/chapter/add',middlewares.validateChapter, controllers.chapter.create);
+app.post('/chapter/add', middlewares.validateChapter, controllers.chapter.create);
 app.get('/chapter/read/:subjectId', controllers.chapter.read);
 app.put('/chapter/update/:id', controllers.chapter.update);
 app.put('/chapter/delete/:id', controllers.chapter.deleteChap);
@@ -29,6 +31,6 @@ app.get('/', (req, res) => {
     res.send("Hello world");
 });
 
-app.listen(3000, () => {
-    console.log("Hello world");
+app.listen(process.env.PORT, () => {
+    console.log(`Server running at port ${process.env.PORT}`);
 });
