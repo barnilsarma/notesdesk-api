c45a8e986e543f31eb3dcfccd30bd3dcf0ee992c
RISHI RAJ SAHA <rishir_ug_22@ece.nits.ac.in>
2024-06-18 07:15:00 +0530

    minor fix

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,4 @@
 import * as library from "./library/index.js";
 import * as subject from "./subject/index.js";
 import * as chapter from "./chapter/index.js";
-import getProfile from "./user.js";
-export { library, subject, chapter, getProfile };
+export { library, subject, chapter};
