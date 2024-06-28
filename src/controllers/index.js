cd5a047a3457beff8801379e3e5a268716a7a77f
RISHI RAJ SAHA <rishir_ug_22@ece.nits.ac.in>
2024-06-28 13:54:29 +0530

    fixes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,5 +2,4 @@ import * as library from "./library/index.js";
 import * as subject from "./subject/index.js";
 import * as chapter from "./chapter/index.js";
 import * as page from "./Page/index.js";
-import getProfile from "./user.js";
-export { library, subject, chapter, getProfile, page };
+export { library, subject, chapter, page };
